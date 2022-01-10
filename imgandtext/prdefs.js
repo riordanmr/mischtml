736dfeda9d65c791f74b597bba854a9388b07547
Mark Riordan <riordan@rocketmail.com>
2022-01-10 10:32:27 -0700

    Fix image styling to auto-expand.  Flesh out sayings even though I don't yet have all images.

--- imgandtext/prdefs.js
+++ imgandtext/prdefs.js
@@ -1,6 +1,18 @@
 var arydefs = [
-    {"img": "primg-1a.PNG", "saying": "Many of us found that we had several characteristics in common as a result of being brought up in an alcoholic or dysfunctional household."},
-    {"img": "primg-2a.PNG", "saying": "We had come to feel isolated and uneasy with other people, especially authority figures."},
-    {"img": "primg-3a.PNG", "saying": "To protect ourselves, we became people-pleasers, even though we lost our own identities in the process."}
-
+    {'img': 'primg-1a.PNG', 'saying': "Many of us found that we had several characteristics in common as a result of being brought up in an alcoholic or dysfunctional household."},
+    {'img': 'primg-2a.PNG', 'saying': "We had come to feel isolated and uneasy with other people, especially authority figures."},
+    {'img': 'primg-3a.PNG', 'saying': "To protect ourselves, we became people-pleasers, even though we lost our own identities in the process."},
+    {'img': 'primg-4a.PNG', 'saying': "All the same we would mistake any personal criticism as a threat."},
+    {'img': 'primg-5c.PNG', 'saying': "We either became alcoholics (or practiced other addictive behavior) ourselves, or married them, or both."},
+    {'img': 'primg-6a.PNG', 'saying': "Failing that, we found other compulsive personalities, such as a workaholic, to fulfill our sick need for abandonment."},
+    {'img': 'primg-7a.PNG', 'saying': "We lived life from the standpoint of victims."},
+    {'img': 'primg-8a.PNG', 'saying': "Having an overdeveloped sense of responsibility, we preferred to be concerned with others rather than ourselves."},
+    {'img': 'primg-9a.PNG', 'saying': "We got guilt feelings when we stood up for ourselves rather than giving in to others."},
+    {'img': 'primg-10a.PNG', 'saying': "Thus, we became reactors, rather than actors, letting others take the initiative."},
+    {'img': 'primg-11a.PNG', 'saying': "We were dependent personalities, terrified of abandonment, willing to do almost anything to hold on to a relationship in order not to be abandoned emotionally."},
+    {'img': 'primg-12a.PNG', 'saying': "Yet we kept choosing insecure relationships because they matched our childhood relationship with alcoholic or dysfunctional parents."},
+    {'img': 'primg-13a.PNG', 'saying': "These symptoms of the family disease of alcoholism or other dysfunction made us 'co-victims', those who take on the characteristics of the disease without necessarily ever taking a drink."},
+    {'img': 'primg-14a.PNG', 'saying': "We learned to keep our feelings down as children and kept them buried as adults."},
+    {'img': 'primg-15a.PNG', 'saying': "As a result of this conditioning, we confused love with pity, tending to love those we could rescue."},
+    {'img': 'primg-16a.PNG', 'saying': "Even more self-defeating, we became addicted to excitement in all our affairs, preferring constant upset to workable relationships."}
 ];
---
+++ imgandtext/txttojs1.awk
@@ -0,0 +1,13 @@
+# txttojs1.awk - simple script to output JavaScript for imgandtext.
+# MRR  2022-01-09
+# Input file contains lines that look like:
+# 1. This is the text associated with Image One.
+#
+# Usage: awk -f txttojs1.awk problem-num.txt
+{
+    line = $0
+    idx = index(line, ".")
+    num = substr(line, 1, idx-1)
+    txt = substr(line, idx+2)
+    print "    {'img': 'primg-" num "a.PNG', 'saying': \"" txt "\"},"
+}
