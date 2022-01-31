646f9246dd1ad3c4c753e8d85a95352dcc4701f5
Mark Riordan <riordan@rocketmail.com>
2022-01-31 14:51:12 -0700

    Tweak which images are best

--- imgandtext/ImageOnly/cvtpng.sh
+++ imgandtext/ImageOnly/cvtpng.sh
@@ -1,1 +1,5 @@
-for i in *.PNG; do sips -s format jpeg $i --out $i.jpg; echo $i.jpg | awk '{origfn=$1; newfn=$1; sub(".PNG","",newfn); cmd="mv " origfn " " newfn; system(cmd)}'; done
+for i in *.PNG; 
+  do sips -s format jpeg $i --out $i.jpg;
+  echo $i.jpg | awk '{origfn=$1; newfn=$1; sub(".PNG","",newfn); cmd="mv " origfn " " newfn; system(cmd)}'; 
+  mv $i pngfiles
+done
--- imgandtext/prdefs.js
+++ imgandtext/prdefs.js
@@ -6,15 +6,15 @@ var arydefs =
     {'img': 'primg-4a.jpg', 'final': true, 'saying': "All the same we would mistake any personal criticism as a threat."},
     {'img': 'primg-5c.jpg', 'final': true, 'saying': "We either became alcoholics (or practiced other addictive behavior) ourselves, or married them, or both."},
     {'img': 'primg-6c.jpg', 'final': true, 'saying': "Failing that, we found other compulsive personalities, such as a workaholic, to fulfill our sick need for abandonment."},
-    {'img': 'primg-7d.jpg', 'final': false, 'saying': "We lived life from the standpoint of victims."},
+    {'img': 'primg-7m.jpg', 'final': false, 'saying': "We lived life from the standpoint of victims."},
     {'img': 'primg-8d.jpg', 'final': true, 'saying': "Having an overdeveloped sense of responsibility, we preferred to be concerned with others rather than ourselves."},
     {'img': 'primg-9a.jpg', 'final': true, 'saying': "We got guilt feelings when we stood up for ourselves rather than giving in to others."},
-    {'img': 'primg-10i.jpg', 'final': false, 'saying': "Thus, we became reactors, rather than actors, letting others take the initiative."},
+    {'img': 'primg-10w.jpg', 'final': true, 'saying': "Thus, we became reactors, rather than actors, letting others take the initiative."},
     {'img': 'primg-11c.jpg', 'final': true, 'saying': "We were dependent personalities, terrified of abandonment, willing to do almost anything to hold on to a relationship in order not to be abandoned emotionally."},
     {'img': 'primg-12b.jpg', 'final': true, 'saying': "Yet we kept choosing insecure relationships because they matched our childhood relationship with alcoholic or dysfunctional parents."},
     {'img': 'primg-13a.jpg', 'final': true, 'saying': "These symptoms of the family disease of alcoholism or other dysfunction made us \"co-victims\", those who take on the characteristics of the disease without necessarily ever taking a drink."},
     {'img': 'primg-14b.jpg', 'final': true, 'saying': "We learned to keep our feelings down as children and kept them buried as adults."},
-    {'img': 'primg-15h.jpg', 'final': false, 'saying': "As a result of this conditioning, we confused love with pity, tending to love those we could rescue."},
+    {'img': 'primg-15j.jpg', 'final': true, 'saying': "As a result of this conditioning, we confused love with pity, tending to love those we could rescue."},
     {'img': 'primg-16e.jpg', 'final': true, 'saying': "Even more self-defeating, we became addicted to excitement in all our affairs, preferring constant upset to workable relationships."},
-    {'img': 'primg-17f.jpg', 'final': false, 'saying': "This is a description, not an indictment."}
+    {'img': 'primg-17i.jpg', 'final': true, 'saying': "This is a description, not an indictment."}
 ];
