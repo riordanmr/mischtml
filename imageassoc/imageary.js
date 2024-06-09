403dc222d09b8e2c0b6ed36beb86423fa10ec36a
Mark Riordan <riordan@rocketmail.com>
2024-06-09 12:52:49 -0700

    Update imageary.js

--- imageassoc/imageary.js
+++ imageassoc/imageary.js
@@ -7,5 +7,11 @@ var aryimages =
        {'img': '180-parents-on-couch.png'},
        {'img': '190-woman-with-dove.png'},
        {'img': '200-standing-alone-at-party.png'},
-       {'img': '210-dismantling-wall.png'}
+       {'img': '210-dismantling-wall.png'},
+       {'img': '220-3-ages-of-woman.png'},
+       {'img': '230-passed-out-drunk-man-and-boy.png'},
+       {'img': '240-woman-draws-while-child-looks-on.png'},
+       {'img': '250-woman-horrified-broken-dish.png'},
+       {'img': '260-man-gambling-liquor.png'},
+       {'img': '270-3-people-smile-and-hold-hands.png'}
 ];
